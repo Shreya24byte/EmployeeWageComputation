3dc40457e4452dfa98e706b41960deb208bcf22d
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-26 14:51:25 +0530

    Add: switch case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,22 +3,27 @@ echo "Welcome to Employee Wage Computation Program";
 
 #Constants
 EMPLOYEE_RATE_PER_HOUR=20;
-FULL_DAY_HOUR=8;
-PART_TIME_HOUR=4;
+FULL_TIME=1;
+PART_TIME=2;
 
 #Attendance check and wage calculation
 attendance=$((RANDOM%3));
-if [ $attendance -eq 1 ];
-then
-        echo "Employee is present";
-	dailyWage=$(($EMPLOYEE_RATE_PER_HOUR * $FULL_DAY_HOUR));
-elif [ $attendance -eq 2 ];
-then
-	echo "Employee is present part time"
-	dailyWage=$(($EMPLOYEE_RATE_PER_HOUR * $PART_TIME_HOUR));
-else
-        echo "Employee is absent";
-	dailyWage=0;
-fi
+case $attendance in
+	$FULL_TIME)
+		workHours=8;
+		dailyWage=$(($EMPLOYEE_RATE_PER_HOUR * $workHours));
+		echo "Employee is present full time"
+		;;
+	$PART_TIME)
+		workHours=4;
+		dailyWage=$(($EMPLOYEE_RATE_PER_HOUR * $workHours));
+		echo "Employee is present part time"
+		;;
+	*)
+		workHours=0;
+		dailyWage=0;
+		echo "Employee is absent"
+		;;
+esac
 
 echo "Employee daily wage:" $dailyWage;
