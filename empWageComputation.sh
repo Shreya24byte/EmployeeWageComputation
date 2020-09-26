b77e0a9a2a2e47b9465d47c262a4ab185a0b73cb
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-26 15:20:30 +0530

    Add: employee monthly wage calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,25 +5,35 @@ echo "Welcome to Employee Wage Computation Program";
 EMPLOYEE_RATE_PER_HOUR=20;
 FULL_TIME=1;
 PART_TIME=2;
+MAX_WORKING_DAYS=20
 
-#Attendance check and wage calculation
-attendance=$((RANDOM%3));
-case $attendance in
-	$FULL_TIME)
-		workHours=8;
-		dailyWage=$(($EMPLOYEE_RATE_PER_HOUR * $workHours));
-		echo "Employee is present full time"
-		;;
-	$PART_TIME)
-		workHours=4;
-		dailyWage=$(($EMPLOYEE_RATE_PER_HOUR * $workHours));
-		echo "Employee is present part time"
-		;;
-	*)
-		workHours=0;
-		dailyWage=0;
-		echo "Employee is absent"
-		;;
-esac
+#Variables
+monthlyWage=0;
 
-echo "Employee daily wage:" $dailyWage;
+#Monthly wage calculation
+for ((day=1; day<=MAX_WORKING_DAYS; day++));
+do
+	#Attendance check and daily wage calculation
+	attendance=$((RANDOM%3));
+	case $attendance in
+		$FULL_TIME)
+			workHours=8;
+			dailyWage=$(($EMPLOYEE_RATE_PER_HOUR * $workHours));
+			echo "Employee is present full time"
+			;;
+		$PART_TIME)
+			workHours=4;
+			dailyWage=$(($EMPLOYEE_RATE_PER_HOUR * $workHours));
+			echo "Employee is present part time"
+			;;
+		*)
+			workHours=0;
+			dailyWage=0;
+			echo "Employee is absent"
+			;;
+	esac
+
+	echo "Employee daily wage:" $dailyWage;
+	monthlyWage=$(($monthlyWage + $dailyWage));
+done
+echo "Employee monthly wage:" $monthlyWage;
