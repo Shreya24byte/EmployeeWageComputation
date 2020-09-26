e5f0b16d1ce0487e15066c5ca22a3e498356fe84
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-26 14:17:27 +0530

    Add: conditional statement to check employee attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program";
 
-
+#employee attendance check
+attendance=$((RANDOM%2));
+if [ $attendance -eq 1 ];
+then
+        echo "Employee is present";
+else
+        echo "Employee is absent";
+fi
