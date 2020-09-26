e053869076c0ded7b0c7e09f257c59b17afb506a
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-26 14:28:53 +0530

    Add: part time employee and wage calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,13 +4,18 @@ echo "Welcome to Employee Wage Computation Program";
 #Constants
 EMPLOYEE_RATE_PER_HOUR=20;
 FULL_DAY_HOUR=8;
+PART_TIME_HOUR=4;
 
 #Attendance check and wage calculation
-attendance=$((RANDOM%2));
+attendance=$((RANDOM%3));
 if [ $attendance -eq 1 ];
 then
         echo "Employee is present";
 	dailyWage=$(($EMPLOYEE_RATE_PER_HOUR * $FULL_DAY_HOUR));
+elif [ $attendance -eq 2 ];
+then
+	echo "Employee is present part time"
+	dailyWage=$(($EMPLOYEE_RATE_PER_HOUR * $PART_TIME_HOUR));
 else
         echo "Employee is absent";
 	dailyWage=0;
