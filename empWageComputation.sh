218850ad8867fb3612ae5d73331b747ab9729aeb
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-26 14:24:04 +0530

    Add: employee daily wage calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program";
 
-#employee attendance check
+#Constants
+EMPLOYEE_RATE_PER_HOUR=20;
+FULL_DAY_HOUR=8;
+
+#Attendance check and wage calculation
 attendance=$((RANDOM%2));
 if [ $attendance -eq 1 ];
 then
         echo "Employee is present";
+	dailyWage=$(($EMPLOYEE_RATE_PER_HOUR * $FULL_DAY_HOUR));
 else
         echo "Employee is absent";
+	dailyWage=0;
 fi
+
+echo "Employee daily wage:" $dailyWage;
